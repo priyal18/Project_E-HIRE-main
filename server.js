c37f93ff117153c7209e9ddf5088795ea948e726
priyal18 <priyal930gupta@gmail.com>
2021-05-13 16:55:27 +0530

    bulid folder

--- server.js
+++ server.js
@@ -52,4 +52,13 @@ io.on("connection", (socket) => {
   });
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 server.listen(process.env.PORT||5000);
